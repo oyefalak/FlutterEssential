43fe48a4dde4665e7ecd03152b1180bfce30ddf5
oyefalak <sabbirrajput03@gmail.com>
2023-01-19 11:10:02 +0530

    Chapter 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,51 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        child: Center(child: Text("Hi Flutter")),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          alignment: Alignment.center,
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 5,
+                offset: Offset(2.0, 5.0),
+              )
+            ],
+            color: Colors.teal,
+            gradient: LinearGradient(
+                colors: [Colors.yellow, Colors.pink],
+            ),
+          ),
+          child: Text("I am a Box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),),
+        ),
       ),
-    );
+        );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
